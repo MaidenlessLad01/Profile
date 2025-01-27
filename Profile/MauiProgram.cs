2e4d3222254b0401158d8416cc0fa4a4f7229b04
MaidenlessLad01 <cortesdemaestro@gmail.com>
2025-01-27 09:22:48 +0800

    kristian gooner

--- Profile/MauiProgram.cs
+++ Profile/MauiProgram.cs
@@ -14,7 +14,9 @@ namespace Profile
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("Cattie-Regular.ttf", "car");
-                    fonts.AddFont("04B30.ttf", "retro");
+                    fonts.AddFont("04B_30__.ttf", "aaa");
+                    fonts.AddFont("Pixels.ttf","pixel");
+                    fonts.AddFont("fontello.ttf", "man");
                 });
 
 #if DEBUG
